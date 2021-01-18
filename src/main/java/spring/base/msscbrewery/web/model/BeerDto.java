0e7eb2ea44dd68976e82c78fc29c6e9b67c64de3
Sooryakumar <sooryakumar.v@heptagon.in>
2021-01-18 15:38:49 +0530

    BeerDto added

---
+++ src/main/java/spring/base/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,23 @@
+package spring.base.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+/*
+* Created by sk @ Jan 18, 21
+* */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+
+    private UUID id;
+    private String beerName;
+    private String beerStyle;
+    private Long upc;
+}
